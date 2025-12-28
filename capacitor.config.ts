7875723a5c4d31bed8808c92fb08f8cd22f2642d
Bavo Bruylandt <bavo.bruylandt@gmail.com>
2025-12-28 18:50:05 +0100

    Add Capacitor Android wrapper

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "app.fieldhockeyrules.agent",
+  appName: "Field Hockey Rules Agent",
+  webDir: "dist",
+};
+
+export default config;
